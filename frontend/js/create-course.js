df9fb1ae5bbb046a6fba0723b52c5bd9c9981b09
AkwireTech <admin@akwiretech.com>
2026-06-03 00:26:10 -0500

    Added academy system and admin course navigation

--- frontend/js/create-course.js
+++ frontend/js/create-course.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // ADMIN PROTECTION
     // ==========================
 
-    if (
+    /*if (
         !user ||
         user.role !== "admin"
     ) {
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         return;
 
-    }
+    }*/
 
     // ==========================
     // CREATE COURSE
