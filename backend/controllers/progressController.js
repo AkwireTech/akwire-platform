fcfd8f669a4e45877c0a5bb238ab6532708c79bb
AkwireTech <admin@akwiretech.com>
2026-06-09 11:15:05 -0500

    Fixed course completion ObjectId comparison

--- backend/controllers/progressController.js
+++ backend/controllers/progressController.js
@@ -1,24 +1,62 @@
 import User from "../models/User.js";
 
 // Mark course as completed
+
 export const completeCourse = async (req, res) => {
+
   try {
-    const user = await User.findById(req.user._id);
 
-    const courseId = req.params.courseId;
+    const user =
+      await User.findById(req.user._id);
 
-    if (!user.progress.includes(courseId)) {
-      user.progress.push(courseId);
+    const courseId =
+      req.params.courseId;
+
+    const alreadyCompleted =
+
+      user.progress.some(
+
+        id =>
+          id.toString() === courseId
+
+      );
+
+    if (!alreadyCompleted) {
+
+      user.progress.push(
+        courseId
+      );
+
       await user.save();
+
     }
 
     res.json({
-      message: "Course marked as completed",
-      progress: user.progress,
+
+      message:
+        "Course marked as completed",
+
+      progress:
+        user.progress
+
     });
+
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    console.error(
+      "COMPLETE COURSE ERROR:",
+      error
+    );
+
+    res.status(500).json({
+
+      message:
+        error.message
+
+    });
+
   }
+
 };
 
 // Get user progress
